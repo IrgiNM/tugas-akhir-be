ba5d4150c5ae258e3fb876368e3acc9791dca7d0
unknown <irginazwamustofa@gmail.com>
2026-07-09 11:53:54 +0700

    fix schedular

--- backend/settings.py
+++ backend/settings.py
@@ -123,22 +123,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ta_project_irgi',
-        'USER': 'postgres',
-        'PASSWORD': '112N4ZMU$',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.environ.get("DATABASE_URL")
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ta_project_irgi',
+#         'USER': 'postgres',
+#         'PASSWORD': '112N4ZMU$',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
 # }
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 
 
